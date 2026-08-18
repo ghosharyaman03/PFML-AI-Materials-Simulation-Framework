d3525d12c0a51b16c7185f36f099021dab41355d
ghosharyaman03 <ghosharyaman03@gmail.com>
2026-08-18 21:15:23 +0530

    Delete microsim_gp directory

--- microsim_gp/tmp_code_71097473b6e95af2df5c899d480c4d07.bash
+++
@@ -1,2 +0,0 @@
-cd /app/microsim_gp/Example_Systems/NiAlMo && \
-mpirun --allow-run-as-root -np 4 /app/microsim_gp/microsim_gp Input_NiAlMo_4.in Filling_NiAlMo_4.in ./DATA 2 2
--- microsim_gp/tmp_code_c654acdbf39304323add406a4dde8718.bash
+++
@@ -1,3 +0,0 @@
-cd Example_Systems/NiAlMo
-mpirun --allow-run-as-root -np 4 /app/microsim_gp/microsim_gp Input_NiAlMo_4.in Filling_NiAlMo_4.in ./simulation_output 2 2
-ls -lh ./simulation_output
